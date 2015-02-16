c4cbd1e9a7754e52d4defc1ff4ac227507fb91b3
David Martin <dwmartin41@gmail.com>
2015-02-16 00:57:57 +0000

    allowed user to watch and star repos. Repo page now complete, resolves #4

--- MyGit/MyGit.Shared/ViewModels/RepositoryPage/RepositoryViewModel.cs
+++ MyGit/MyGit.Shared/ViewModels/RepositoryPage/RepositoryViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
 using Octokit;
 
 namespace MyGit.ViewModels.RepositoryPage
@@ -14,6 +16,32 @@ namespace MyGit.ViewModels.RepositoryPage
             {
                 _repository = value; 
                 OnPropertyChanged();
+            }
+        }
+
+        private bool _isWatched;
+
+        public bool IsWatched
+        {
+            get { return _isWatched; }
+            set
+            {
+                _isWatched = value;
+                OnPropertyChanged();
+                ToggleWatch.RaiseCanExecuteChanged();
+            }
+        }
+
+        private bool _isStarred;
+
+        public bool IsStarred
+        {
+            get { return _isStarred; }
+            set
+            {
+                _isStarred = value;
+                OnPropertyChanged();
+                ToggleStarred.RaiseCanExecuteChanged();
             }
         }
 
@@ -43,7 +71,94 @@ namespace MyGit.ViewModels.RepositoryPage
         public override async Task Refresh()
         {
             Repository = await GitHubClient.Repository.Get(_owner, _name);
+            await CheckSubscriptionStatus();
             await  Task.WhenAll(IssuesViewModel.Refresh(), PullRequestsViewModel.Refresh(), CommitsViewModel.Refresh());
+        }
+
+        public async Task CheckSubscriptionStatus()
+        {
+            SubscriptionInfoUpdating = true;
+            IsWatched = await GitHubClient.Activity.Watching.CheckWatched(Owner, Repo);
+            IsStarred = await GitHubClient.Activity.Starring.CheckStarred(Owner, Repo);
+            SubscriptionInfoUpdating = false;
+        }
+
+        private bool _subscriptionInfoUpdating = true;
+
+        public bool SubscriptionInfoUpdating
+        {
+            get { return _subscriptionInfoUpdating; }
+            set
+            {
+                _subscriptionInfoUpdating = value;
+                OnPropertyChanged();
+                ToggleWatch.RaiseCanExecuteChanged();
+                ToggleStarred.RaiseCanExecuteChanged();
+            }
+        }
+
+        private DelegateCommand _toggleWatch;
+        public DelegateCommand ToggleWatch
+        {
+            get
+            {
+                if (_toggleWatch == null)
+                {
+                    _toggleWatch = new DelegateCommand(async () =>
+                    {
+                        SubscriptionInfoUpdating = true;
+                        if (IsWatched)
+                        {
+                            await
+                                GitHubClient.Activity.Watching.UnwatchRepo(Owner, Repo)
+                                    .ContinueWith(t => CheckSubscriptionStatus());
+                        }
+                        else
+                        {
+                            await
+                                GitHubClient.Activity.Watching.WatchRepo(Owner, Repo, new NewSubscription())
+                                    .ContinueWith(t => CheckSubscriptionStatus());
+                        }
+
+                        SubscriptionInfoUpdating = false;
+                        IsWatched = !IsWatched;
+                    },
+                        () => !SubscriptionInfoUpdating);
+                }
+                return _toggleWatch;
+            }
+        }
+
+        private DelegateCommand _toggleStarred;
+        public DelegateCommand ToggleStarred
+        {
+            get
+            {
+                if (_toggleStarred == null)
+                {
+                    _toggleStarred = new DelegateCommand(async () =>
+                    {
+                        SubscriptionInfoUpdating = true;
+                        if (IsStarred)
+                        {
+                            await
+                                GitHubClient.Activity.Starring.RemoveStarFromRepo(Owner, Repo)
+                                    .ContinueWith(t => CheckSubscriptionStatus());
+                        }
+                        else
+                        {
+                            await
+                                GitHubClient.Activity.Starring.StarRepo(Owner, Repo)
+                                    .ContinueWith(t => CheckSubscriptionStatus());
+                        }
+
+                        SubscriptionInfoUpdating = false;
+                        IsStarred = !IsStarred;
+                    },
+                        () => !SubscriptionInfoUpdating);
+                }
+                return _toggleStarred;
+            }
         }
     }
 }
---
+++ MyGit/MyGit.Shared/ViewModels/UserDetailsPage/UserDetailsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Octokit;
+
+namespace MyGit.ViewModels.UserDetailsPage
+{
+    public class UserDetailsViewModel : BaseViewModel
+    {
+        private User _user;
+
+        public User User
+        {
+            get { return _user; }
+            set
+            {
+                _user = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public UserDetailsViewModel()
+        {
+            this.RefreshInternal();
+        }
+
+        public override async Task Refresh()
+        {
+            User = await GitHubClient.User.Current();
+        }
+    }
+}
