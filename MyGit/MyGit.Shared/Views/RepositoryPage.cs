4e6ae5134f5b33ac5075f93976986db51295e8d8
David Martin <dwmartin41@gmail.com>
2015-02-17 22:38:49 +0000

    added link to parent on repo page

--- MyGit/MyGit.Shared/Views/RepositoryPage.cs
+++ MyGit/MyGit.Shared/Views/RepositoryPage.cs
@@ -1,4 +1,7 @@
-﻿namespace MyGit.Views
+﻿using Windows.UI.Xaml.Documents;
+using MyGit.ViewModels.RepositoryPage;
+
+namespace MyGit.Views
 {
     public partial class RepositoryPage
     {
@@ -6,6 +9,16 @@
         {
             public string Owner;
             public string Name;
+        }
+
+        private void ForkedFromLink_OnClick(Hyperlink sender, HyperlinkClickEventArgs args)
+        {
+            var parent = (this.DataContext as RepositoryViewModel).Repository.Parent;
+            App.Frame.Navigate(typeof (RepositoryPage), new RepositoryPageParameters
+            {
+                Name = parent.Name,
+                Owner = parent.Owner.Login
+            });
         }
     }
 }
