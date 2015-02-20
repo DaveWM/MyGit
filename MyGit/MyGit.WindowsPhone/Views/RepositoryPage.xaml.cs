3caf5a026369910ddb6299b406acd2eb5c25c923
David Martin <dwmartin41@gmail.com>
2015-02-20 01:34:56 +0000

    enabled page caching for repo page

--- MyGit/MyGit.WindowsPhone/Views/RepositoryPage.xaml.cs
+++ MyGit/MyGit.WindowsPhone/Views/RepositoryPage.xaml.cs
@@ -14,17 +14,8 @@ namespace MyGit.Views
         public RepositoryPage()
         {
             this.InitializeComponent();
-        }
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            var parameters = e.Parameter as RepositoryPageParameters;
-            this.DataContext = new RepositoryViewModel(parameters.Owner, parameters.Name);
+            this.NavigationCacheMode = NavigationCacheMode.Enabled;
         }
     }
 }
