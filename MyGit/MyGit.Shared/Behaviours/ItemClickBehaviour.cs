eeb8756a10052ffab56c0c4303ff4fcf9f3c5af1
David Martin <dwmartin41@gmail.com>
2015-02-20 02:58:51 +0000

    hooked up notification click to open issue

--- MyGit/MyGit.Shared/Behaviours/ItemClickBehaviour.cs
+++ MyGit/MyGit.Shared/Behaviours/ItemClickBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml;
+﻿using System.Text.RegularExpressions;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
 using Microsoft.Practices.Unity;
@@ -55,6 +56,56 @@ namespace MyGit.Behaviours
                     {
                         client.Notification.MarkAsRead(int.Parse(notification.Id));
                     }
+
+                    switch (notification.Subject.Type.ToLower())
+                    {
+                        case "issue":
+                            // notification doesn't get issue number, have to parse from url
+                            var numberRegex = new Regex(@"(?<=\/)[^\/]*\Z");
+                            var number = int.Parse(numberRegex.Match(notification.Subject.Url).Value);
+                            var issueParams = new IssuePage.IssuePageParameters
+                            {
+                                Number = number,
+                                Owner = notification.Repository.Owner.Login,
+                                Repo = notification.Repository.Name
+                            };
+                            App.Frame.Navigate(typeof (IssuePage), issueParams);
+                            break;
+                    }
+                };
+            }
+        }
+
+
+        public static readonly DependencyProperty NavigateToIssue = DependencyProperty.RegisterAttached(
+            "ShouldGoToIssue", typeof(bool), typeof(ItemClickBehaviour), new PropertyMetadata(false));
+
+        public static bool GetShouldGoToIssue(Grid grid)
+        {
+            return (bool)grid.GetValue(NavigateToIssue);
+        }
+
+        public static void SetShouldGoToIssue(Grid grid, bool value)
+        {
+            if (value)
+            {
+                grid.Tapped += (s, e) =>
+                {
+                    var issue = ((FrameworkElement)s).DataContext as Issue;
+
+                    // issue doesn't have repo info in the api, even though we need the repo name and owner to get the single issue ಠ_ಠ
+                    // have to parse repo name from issue url ಠ_ಠ
+                    var nameRegex = new Regex(@"(?<=\/)[^\/]*(?=\/issues)");
+                    var repoName = nameRegex.Match(issue.Url.AbsolutePath).Value;
+                    var ownerRegex = new Regex(@"(?<=\/)[^\/]*(?=\/[^\/]*\/issues)");
+                    var owner = ownerRegex.Match(issue.Url.AbsolutePath).Value;
+
+                    App.Frame.Navigate(typeof(IssuePage), new IssuePage.IssuePageParameters
+                    {
+                        Repo = repoName,
+                        Number = issue.Number,
+                        Owner = owner
+                    });
                 };
             }
         }
